3ab160c78c7b85bcd6bc7b39febdef7a0d174242
inthiyazahmed <inthiyazahmed9@gmail.com>
2024-03-20 10:35:52 +0530

    committt1

---
+++ dev.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class a
+{public:
+    void get()
+    {
+        cout << "this is aaaa";
+    }
+};
+class b
+{public:
+    void put(){
+        cout << "this is bbbb";
+    }
+};
+class c:public a,public b{
+    public:
+    void print(){
+        cout<<"this is cccc";
+    }
+};
+int main(){
+    c obj;
+    obj.get();
+    obj.put();
+    obj.print();
+    return 0;
+}
